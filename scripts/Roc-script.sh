73ab5e865107d3a703aa1e0053d6c1d6cf0cc288
Lang-Ke <80813300+Lang-Ke@users.noreply.github.com>
2026-01-07 22:05:25 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -70,6 +70,9 @@ git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-a
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+rm -rf package/momo
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo package/momo
+
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 ### PassWall & OpenClash ###
